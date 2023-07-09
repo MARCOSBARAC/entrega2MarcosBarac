f6503a4e2c5d02fa7a7e102333fa26b0c7ea81f7
Marcos Barac <marcos.barac@gmail.com>
2023-07-09 20:59:37 -0300

    se modifica funcion precio con cuotas para trabajar con objetos de array y se corrige el array para poder utilizar los valores de stock y precio

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-class siluetas {
+class silueta {
   constructor(marca, modelo, talle, precio, stock) {
     this.marca = marca;
     this.modelo = modelo;
@@ -6,45 +6,37 @@ class siluetas {
     this.precio = precio;
     this.stock = stock;
   }
-  venta() {
-    this.stock--;
-  }
 }
-const travisScott = new siluetas(
+
+const travisScott = new silueta(
   "Jordan",
   "1 High x Travis Scott",
   "12 US",
-  "US$ 1275",
-  "1"
+  1275,
+  1
 );
-const yeezy700 = new siluetas(
-  "Yeezy",
-  "700 Waveruner",
-  "11.5 US",
-  "US$ 334",
-  "1"
-);
-const dunk = new siluetas("Nike", "Dunk What The Paul", "9 US", "US$ 471", "1");
-const jordanLost = new siluetas(
+const yeezy700 = new silueta("Yeezy", "700 Waveruner", "11.5 US", 334, 1);
+const dunk = new silueta("Nike", "Dunk What The Paul", "9 US", 471, 1);
+const jordanLost = new silueta(
   "Jordan",
   "1 Chicago Lost n Found",
   "9.5 US",
-  "US$ 422",
-  "2"
+  422,
+  2
 );
-const forumBadBunny = new siluetas(
+const forumBadBunny = new silueta(
   "Adidas",
   "Forum x Bad Bunny",
   "9 US",
-  "US$ 471",
-  "1"
+  471,
+  1
 );
-const airMax97 = new siluetas(
+const airMax97 = new silueta(
   "Nike",
   "Airmax 97 Have a Nike Day",
   "12 US",
-  "US$ 370",
-  "1"
+  370,
+  1
 );
 const myArray = [
   travisScott,
@@ -55,28 +47,35 @@ const myArray = [
   airMax97,
 ];
 
-travisScott;
-travisScott.venta();
-console.log(travisScott);
-
 function cuotasValidas(num) {
-  return num == 3 || num == 6 || num == 12;
+  return num == 1 || num == 3 || num == 6 || num == 12;
+}
+
+function ventaDePar(_silueta) {
+  const par = myArray.find((i) => i.modelo == _silueta);
+  par.stock = par.stock - 1;
 }
 
 function precioConCuotas() {
-  let importeAbonar = prompt("Importe a abonar");
+  let par = prompt("Ingrese el par a comprar");
+  ventaDePar(par);
+  let importeAbonar = myArray.find((i) => i.modelo == par).precio;
   let cantidadCuotas = prompt("Cuantas cuotas?");
   while (!cuotasValidas(cantidadCuotas)) {
     alert("El numero de cuotas no es correcto");
     cantidadCuotas = prompt("Cuantas cuotas?");
   }
-  if (cantidadCuotas == 3) {
-    alert("El importe a abonar es: " + Number(importeAbonar * 1.25));
-  } else if (cantidadCuotas == 6) {
-    alert("El importe a abonar es: " + Number(importeAbonar * 1.5));
-  } else if (cantidadCuotas == 12) {
-    alert("El importe a abonar es: " + Number(importeAbonar * 2));
+  if (cantidadCuotas == 1) {
+    alert("El importe a abonar es: " + importeAbonar * 1);
   }
+  if (cantidadCuotas == 3) {
+    alert("El importe a abonar es: " + importeAbonar * 1.25);
+  } else if (cantidadCuotas == 6) {
+    alert("El importe a abonar es: " + importeAbonar * 1.5);
+  } else if (cantidadCuotas == 12) {
+    alert("El importe a abonar es: " + importeAbonar * 2);
+  }
+  console.log(myArray.find((i) => i.modelo == par).stock);
 }
 
 precioConCuotas();
